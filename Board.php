eb3b5b0619dd18a7e47f5475dcf660d06e8bdafa
agsaghhk <agsaghhk@yahoo.com>
2016-07-28 10:00:57 -0700

    Finished tictactoe

--- Board.php
+++ Board.php
@@ -5,6 +5,48 @@
    $boards=array(array(0,0,0,0,0),array(0,0,0,0,0),array(0,0,0,0,0),array(0,0,0,0,0),array(0,0,0,0,0));
 
 
+    #Code used to print out all elements of board in board format
+    for($z=0; $z<count($boards); $z++){
+       if($z%2===1){
+          echo "-----\n";
+       }
+
+       else{
+          
+	  for($Z=0; $Z<count($boards[$z]) ; $Z++){
+		    
+	     if($Z%2===1){
+                
+		echo "|";
+		    
+             }
+	     
+	     else{
+		       
+	        if($boards[$z][$Z]==1){
+		          
+			  echo "X";
+		
+		}
+		
+		else if($boards[$z][$Z]==2){
+		   
+		   echo "O";
+		      
+		}
+		
+		else{
+		   
+		   echo " ";
+		
+		}
+             }
+	  }
+          
+	  echo "\n";
+       }
+    }
+   
    #parse movements for 5 part array
    $playerTurn=1;
    fprintf(STDOUT,"Player %d make a move:",$playerTurn);
@@ -12,6 +54,7 @@
    #get users move
    $move=fgets(STDIN);
    
+   #if user enters q exit game loop
    while(substr($move,0,1)!="q"){
 	   
 	   #used to determine if move was invalid
@@ -38,6 +81,7 @@
 	   
 	   }
 	   
+	   #if user doesnt enter in the form #,# print error
 	   elseif(substr($move,1,1)!=","){
 	      
 	      fwrite(STDERR, "Please enter move in the form #,#\n");
@@ -185,95 +229,201 @@
 
 	   #Code used to print out all elements of board in board format
 	   for($z=0; $z<count($boards); $z++){
+	      
+	      #every other row print space seperation
 	      if($z%2===1){
+		 
 		 echo "-----\n";
+	      
 	      }
+	      
+	      #otherwise print characters
 	      else{
+		 
 		 for($Z=0; $Z<count($boards[$z]) ; $Z++){
+		    
+		    #every other space print board seperation
 		    if($Z%2===1){
+		       
 		       echo "|";
+		    
 		    }
+		    
 		    else{
 		       
+		       #if player one print X
 		       if($boards[$z][$Z]==1){
-		          echo "X";
+		          
+			  echo "X";
+		       
 		       }
+		       
+		       #if player two prin O
 		       else if($boards[$z][$Z]==2){
-		          echo "O";
+		          
+			  echo "O";
+		       
 		       }
+		       
+		       #if unoccupied print space
 		       else{
-		          echo " ";
+		          
+			  echo " ";
+		       
 		       }
+		    
 		    }
+		 
 		 }
-		 echo "\n";
+		
+		#print new line to create rows
+		echo "\n";
+	      
 	      }
+	   
 	   }
 
-           if($invalid!=1){
+           #if valid move
+	   if($invalid!=1){
+	      
+	      #if player two set turn to 2
 	      if(($playerTurn+1)%2==0){
-	         $playerTurn=2;
+	         
+		 $playerTurn=2;
+	      
 	      }
+	      
+	      #otherwise set turn to 1
 	      else{
-	         $playerTurn=1;
+	         
+		 $playerTurn=1;
+	      
 	      }
+	   
 	   }   
 	   
+	   #whole block of game over checks
+
+	   #if first space occupied
 	   if($boards[$space1][$space1]!=0){
+	      
+	      #store the space value
 	      $check=$boards[$space1][$space1];
+	      
+	      #if three in a row to the right game over
 	      if($boards[$space1][$space2]==$check And $boards[$space1][$space3]==$check){
-	         echo "Game Over player $check wins\n";
+	         
+		 echo "Game Over player $check wins\n";
 		 exit;
+	      
 	      }
+
+	      #if three diagonally game over
 	      elseif($boards[$space2][$space2]==$check And $boards[$space3][$space3]==$check){
-	         echo "Game Over player $check wins\n";
+	         
+		 echo "Game Over player $check wins\n";
 		 exit;
-              }
+              
+	      }
+	      
+	      #if three vertically game over
 	      elseif($boards[$space2][$space1]==$check And $boards[$space3][$space1]==$check){
-	         echo "Game Over player $check wins\n";
+	         
+		 echo "Game Over player $check wins\n";
 		 exit;
+	      
 	      }
+	   
 	   }
+	   
+	   #if second space occupied
 	   if($boards[$space1][$space2]!=0){
+	      
+	      #store space value
 	      $check=$boards[$space1][$space2];
+	      
+	      #if three vertically game over
 	      if($boards[$space2][$space2]==$check And $boards[$space3][$space2]==$check){
-	         echo "Game Over player $check wins\n";
+	         
+		 echo "Game Over player $check wins\n";
 		 exit;
-              }
+              
+	      }
+	   
 	   }
+	   
+	   #if third space occupied
 	   if($boards[$space1][$space3]!=0){
+	      
+	      #store space value
 	      $check=$boards[$space1][$space3];
+	      
+	      #if three vertically game over
 	      if($boards[$space2][$space3]==$check And $boards[$space3][$space3]==$check){
-	         echo "Game Over player $check wins\n";
+	         
+		 echo "Game Over player $check wins\n";
 		 exit;
+	      
 	      }
+	      
+	      #if three diagonally game over
 	      if($boards[$space2][$space2]==$check And $boards[$space3][$space1]==$check){
-	         echo "Game Over player $check wins\n";
+	         
+		 echo "Game Over player $check wins\n";
 		 exit;
+	      
 	      }
+	   
 	   }
+	   
+	   #if first space of second row occupied
 	   if($boards[$space2][$space1]!=0){
+	      
+	      #store space value
 	      $check=$boards[$space2][$space1];
+	      
+	      #if three horizontally game over
 	      if($boards[$space2][$space2]==$check And $boards[$space2][$space3]==$check){
-	         echo "Game over player $check wins\n";
+	         
+		 echo "Game over player $check wins\n";
 		 exit;
-              }
-	   }
-	   if($boards[$space3][$space1]!=0){
-	      $check=$boards[$space3][$space1];
-	      if($boards[$space3][$space2]==$check And $boards[$space3][$space3]==$check){
-	         echo "Game Over player $check wins\n";
-		 exit;
+              
 	      }
+	   
 	   }
-	   if($boards[$space1][$space1]!=0 And $boards[$space1][$space2]!=0 And $boards[$space1][$space3]!=0 And
-	      $boards[$space2][$space1]!=0 And $boards[$space2][$space2]!=0 And $boards[$space2][$space3]!=0 And
-	      $boards[$space3][$space1]!=0 And $boards[$space3][$space2]!=0 And $boards[$space3][$space3]!=0){
+	   
+	   #if first space of third row occupied
+	   if($boards[$space3][$space1]!=0){
+	      
+	      #store space value
+	      $check=$boards[$space3][$space1];
+	      
+	      #if three horizontally game over
+	      if($boards[$space3][$space2]==$check And $boards[$space3][$space3]==$check){
+	         
+		 echo "Game Over player $check wins\n";
+		 exit;
+	      
+	      }
+	   
+	   }
+	   
+	   #if cat game game over
+	   if($boards[$space1][$space1]!=0 And $boards[$space1][$space2]!=0 
+	     And $boards[$space1][$space3]!=0 And $boards[$space2][$space1]!=0 
+	     And $boards[$space2][$space2]!=0 And $boards[$space2][$space3]!=0
+	     And $boards[$space3][$space1]!=0 And $boards[$space3][$space2]!=0 
+	     And $boards[$space3][$space3]!=0){
+	      
 	      echo "Game Over. It's a tie.\n";
 	      exit;
+	   
 	   }
+	   
+	   #if all game over checks false then go to next turn
 	   fprintf(STDOUT,"Player %d make a move:",$playerTurn);
 	   $move=fgets(STDIN);
+	
 	}
 
 
